6cd397ce93d16a307411abd96ff7b9ffb4a5c1e3
Jair <jairaceves56@gmail.com>
2023-08-18 17:18:24 -0600

    Agregamos un nuevo Personaje

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -10,14 +10,18 @@ export class MainPageComponent {
 
 
     public characters: Character[] = [{
-        name: 'Krillin',
-        power: 1000
-    },
-    {
-        name: 'Goku',
-        power: 9500
-    }
-]
+            name: 'Krillin',
+            power: 1000
+        },
+        {
+            name: 'Goku',
+            power: 9500
+        },
+        {
+            name: 'Vegeta',
+            power: 7500
+        }
+    ]
 
     
 }
